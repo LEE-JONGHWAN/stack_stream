3cbfe7a836efe1a6a0f35544e6c849eeb2857016
Lee, JongHwan <jong479@gmail.com>
2021-08-19 17:57:59 +0900

    updated first commit

---
+++ src/sec01/stream_introduction/MapAndReduceExample.java
@@ -0,0 +1,27 @@
+﻿package sec01.stream_introduction;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class MapAndReduceExample {
+	public static void main(String[] args) {
+		List<Student> studentList = Arrays.asList(
+				new Student("홍길동", 10),
+				new Student("신용권", 20),
+				new Student("유미선", 31)
+		);		
+			
+		try {
+		double avg = studentList.stream()
+			//중간처리(학생 객체를 점수로 매핑)
+			.mapToInt(Student :: getScore)
+			//최종 처리(평균 점수)
+			.average().getAsDouble();
+		
+			System.out.println("평균 점수: " + avg);
+		} catch(NoSuchElementException e) {
+		System.out.println("평균 점수: (해당 없음) ");
+		}
+	}
+}
---
+++ src/sec02/stream_kind/FromFileContentExample.java
@@ -0,0 +1,33 @@
+﻿package sec02.stream_kind;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class FromFileContentExample {
+	public static void main(String[] args) throws IOException {
+		Path path = Paths.get("src/sec02/stream_kind/linedata.txt");
+		
+		//Files.lines() 메소드 이용
+		try(Stream<String> stream = Files.lines(path, Charset.defaultCharset())) {
+		stream.forEach( System.out :: println );
+		stream.close();
+		System.out.println();
+		}
+		//BufferedReader의 lines() 메소드 이용
+		File file = path.toFile();
+		try (FileReader fileReader = new FileReader(file);
+		BufferedReader br = new BufferedReader(fileReader);
+			Stream<String>	stream2 = br.lines()) {
+		stream2.forEach( System.out :: println );
+		stream2.close();
+			}
+
+	}
+}
